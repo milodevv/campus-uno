fca871ffd2cde9df4dbbe20b96b24dcbf989fd09
milodev <milodev09@gmail.com>
2025-08-07 04:49:13 -0500

    add: services to persistence layer

---
+++ src/Infrastructure/campusuno.Persistence/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using campusuno.UseCases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campusuno.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+            });
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
