a67cb1ad86723ed19f65ce23861d231e0c327014
milodev <milodev09@gmail.com>
2025-08-08 23:43:57 -0500

    add: services for di

--- src/Infrastructure/campusuno.Persistence/DependencyInjection.cs
+++ src/Infrastructure/campusuno.Persistence/DependencyInjection.cs
@@ -3,12 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace campusuno.Persistence
 {
