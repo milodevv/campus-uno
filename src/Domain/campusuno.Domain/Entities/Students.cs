259476f51c8c5fd287ed514035f3c09d463c8d6c
milodev <milodev09@gmail.com>
2025-08-09 23:51:32 -0500

    add: students entity

---
+++ src/Domain/campusuno.Domain/Entities/Students.cs
@@ -0,0 +1,15 @@
+﻿using campusuno.Domain.Commons;
+
+namespace campusuno.Domain.Entities
+{
+    public class Students : BaseAuditableEntity
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
