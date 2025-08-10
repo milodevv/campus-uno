4eb14beb300d214b88e01c91de6ba5e76a2a84cc
milodev <milodev09@gmail.com>
2025-08-10 00:36:57 -0500

    add: create students table

---
+++ src/Infrastructure/campusuno.Persistence/Migrations/20250810053550_CreateStudentsTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace campusuno.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateStudentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    CreateAt = table.Column<DateTime>(type: "DATETIME", nullable: false, defaultValueSql: "GETDATE()"),
+                    CreateBy = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    UpdateBy = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
