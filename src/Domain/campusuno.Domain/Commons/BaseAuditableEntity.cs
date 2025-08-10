afde16f4a957ef54a943d4d4f24859d3e763fb76
milodev <milodev09@gmail.com>
2025-08-09 23:51:13 -0500

    add: base auditable entity

---
+++ src/Domain/campusuno.Domain/Commons/BaseAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campusuno.Domain.Commons
+{
+    public class BaseAuditableEntity
+    {
+        public DateTime CreateAt { get; set; }
+        public Guid CreateBy { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public Guid UpdateBy { get; set; }
+    }
+}
