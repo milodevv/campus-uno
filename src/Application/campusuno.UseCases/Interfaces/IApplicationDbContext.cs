6b4d63ea4df3a66dddfce1163a993c64781cb4f0
milodev <milodev09@gmail.com>
2025-08-09 23:52:11 -0500

    add: db set students

--- src/Application/campusuno.UseCases/Interfaces/IApplicationDbContext.cs
+++ src/Application/campusuno.UseCases/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,10 @@
-﻿namespace campusuno.UseCases;
+﻿using campusuno.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace campusuno.UseCases;
 
 public interface IApplicationDbContext
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    DbSet<Students> Students { get; set; }
 }
